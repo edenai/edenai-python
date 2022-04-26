459bc4689e7a37e4073c9a379ded081d99e422ba
Samy <samy@edenai.co>
2022-04-26 12:53:07 +0200

    [fix] push tests settings

---
+++ test/settings.py
@@ -0,0 +1,8 @@
+import os 
+
+TEST_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(TEST_BASE_DIR, "data/")
+
+PDF_FILE = os.path.join(DATA_DIR, "test.pdf")
+IMAGE_1 = os.path.join(DATA_DIR, "image.jpg")
+AUDIO_FILE = os.path.join(DATA_DIR, "audio.mp3")
