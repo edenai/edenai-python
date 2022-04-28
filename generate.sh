c455ab07fe2b8769ab4b245ffe8d916c1420085c
Samy <samy@edenai.co>
2022-04-28 11:18:04 +0200

    [fix] generate.sh for ci

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-sudo docker run --net=host --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate  -i http://localhost/v1/swagger.json -c /local/config.json -l python -o /local 
+sudo docker run --net=host --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate  -i /local/swagger.json -c /local/config.json -l python -o /local 
 # sudo docker run  --net=host --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://api.edenai.run/v1/swagger.json -c /local/config.json -g python -o /local 
 sudo chown -R sam:sam .
 bash simplify.sh
